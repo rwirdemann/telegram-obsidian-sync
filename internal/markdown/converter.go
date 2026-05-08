d77d5e8f959b1d6e49e14b1f87f2c16e5624f59b
Ralf Wirdemann <ralf.wirdemann@mailbox.org>
2026-05-08 10:23:53 +0200

    refactor: remove heading from note output (#1)
    Notes now contain only the message text, no date header.

--- internal/markdown/converter.go
+++ internal/markdown/converter.go
@@ -1,7 +1,6 @@
 package markdown
 
 import (
-	"fmt"
 	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -17,7 +16,6 @@ type Note struct {
 func Convert(msg *tgbotapi.Message) Note {
 	t := time.Unix(int64(msg.Date), 0).Local()
 	filename := t.Format("2006-01-02-150405") + ".md"
-	title := t.Format("2006-01-02 15:04")
-	content := fmt.Sprintf("# %s\n\n%s\n", title, msg.Text)
+	content := msg.Text + "\n"
 	return Note{Filename: filename, Content: content}
 }
